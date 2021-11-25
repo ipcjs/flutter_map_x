12130978d2061c23da76250e40764169773895f3
limingnie <limingnie@transcodegroup.com>
2021-11-25 13:42:44 +0800

    新增android下百度地图的配置

--- lib/bmf_map_view.dart
+++ lib/bmf_map_view.dart
@@ -1,8 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map_x/flutter_map_x.dart';
+import 'package:flutter_baidu_mapapi_map/flutter_baidu_mapapi_map.dart';
+import 'package:flutter_baidu_mapapi_base/flutter_baidu_mapapi_base.dart';
 
 class BmfMapView implements MapView {
   @override
-  Widget get mapView => const Text("sf");
-  
+  Widget get mapView => BMFMapWidget(
+        onBMFMapCreated: onBMFMapCreated,
+        mapOptions: _initMapOptions(),
+      );
+
+  late BMFMapController myMapController;
+
+  /// 设置地图参数
+  BMFMapOptions _initMapOptions() {
+    BMFMapOptions mapOptions = BMFMapOptions(
+      center: BMFCoordinate(39.917215, 116.380341),
+      zoomLevel: 12,
+    );
+    return mapOptions;
+  }
+
+  /// 创建完成回调
+  void onBMFMapCreated(BMFMapController controller) {
+    myMapController = controller;
+
+    /// 地图加载回调
+    myMapController.setMapDidLoadCallback(callback: () {
+      print('mapDidLoad-地图加载完成');
+    });
+  }
 }
--- lib/gms_map_view.dart
+++ lib/gms_map_view.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map_x/flutter_map_x.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class GmsMapView implements MapView {
   @override
-  Widget get mapView => const Text("df");
-  
+  Widget get mapView => GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: const CameraPosition(
+          target: LatLng(
+            37.42796133580664,
+            -122.085749655962,
+          ),
+          zoom: 14.4746,
+        ),
+        onMapCreated: (GoogleMapController controller) {},
+      );
 }
